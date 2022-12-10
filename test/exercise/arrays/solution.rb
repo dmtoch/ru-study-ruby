503377cec3b3f138b59cce73142985d6b466c621
Daria Tochenaya <d.tochenaya@gmail.com>
2022-12-10 18:32:45 +0300

    added solution to replace task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,8 +1,19 @@
 module Exercise
   module Arrays
     class << self
+      def get_max_value(array)
+        return nil if array.empty?
+
+        max_value = array.first
+        array.each do |elem|
+          max_value = elem if max_value < elem
+        end
+        max_value
+      end
+
       def replace(array)
-        array
+        max_value = get_max_value(array)
+        array.map { |elem| elem.positive? ? max_value : elem }
       end
 
       def search(_array, _query)
