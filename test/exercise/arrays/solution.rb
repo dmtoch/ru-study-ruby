1772e629a6a007a68bfcd4203f171dcbcd5dd2dc
Daria Tochenaya <d.tochenaya@gmail.com>
2022-12-12 15:13:33 +0300

    added solution to search task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -16,8 +16,25 @@ module Exercise
         array.map { |elem| elem.positive? ? max_value : elem }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        binary_search(array, query, 0, array.length - 1)
+      end
+
+      def binary_search(array, query, start_index, end_index)
+        return -1 if array.empty?
+
+        if start_index == end_index
+          return -1 if array[start_index] != query
+          return start_index if array[start_index] == query
+        end
+
+        middle_index = (start_index + end_index) / 2
+
+        if array[middle_index] >= query
+          binary_search(array, query, start_index, middle_index)
+        elsif array[middle_index] < query
+          binary_search(array, query, middle_index + 1, end_index)
+        end
       end
     end
   end
